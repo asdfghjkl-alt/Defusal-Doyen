2033fe8c2b37fd90e04d17e9e20823ad6774cdef
Edward Liu <edwardliu@edwards-air.home>
2024-04-28 18:40:14 +1000

    Preparing for merge

---
+++ Assets/Scripts/GUI Elements/DisplayTime.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayTime : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        int roundedTimer = (int)StaticData.timer;
+
+        // Calculation for minutes and seconds
+        int minutes = roundedTimer / 60;
+        int seconds = roundedTimer % 60;
+        
+        // Displaying the minutes and seconds
+        timerText.text = minutes.ToString() + "MIN " + seconds.ToString() + "S";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
