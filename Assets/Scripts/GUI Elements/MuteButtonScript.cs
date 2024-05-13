58d9926fa23321a250a123309a33392c378bba37
Edward Liu <edwardliu@Edwards-MacBook-Air.local>
2024-05-14 08:59:17 +1000

    Added comments for audio manager and mute button script

--- Assets/Scripts/GUI Elements/MuteButtonScript.cs
+++ Assets/Scripts/GUI Elements/MuteButtonScript.cs
@@ -3,11 +3,15 @@ using UnityEngine.UI;
 
 public class MuteButton : MonoBehaviour
 {
+    // Sprites for a muted and unmuted button
     [SerializeField] private Sprite MutedSprite;
     [SerializeField] private Sprite UnmutedSprite;
+
+    // Gets image component of the button
     [SerializeField] private Image ImageRendRef;
 
     void Start() {
+        // Changes sprite based on whether the sound is on or off
         if (StaticData.soundOn) {
             ImageRendRef.sprite = UnmutedSprite;
         } else {
@@ -16,13 +20,18 @@ public class MuteButton : MonoBehaviour
     }
 
     public void ChangeBGMusic() {
+        // Plays a click sound
         FindObjectOfType<AudioManager>().PlaySound("Click");
+
+        // Whether sound is on is changed to the opposite value
         StaticData.soundOn = !StaticData.soundOn;
 
         if (StaticData.soundOn) {
+            // If the sound is on, then play sound and change sprite
             FindObjectOfType<AudioManager>().PlaySound("Background Sound");
             ImageRendRef.sprite = UnmutedSprite;
         } else {
+            // If sound off, then play sound and change sprite
             FindObjectOfType<AudioManager>().StopSound("Background Sound");
             ImageRendRef.sprite = MutedSprite;
         }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    // Gets array of sounds (Assigned in Unity Editor)
     public Sound[] Sounds;
 
     // Variable only here to check that there is only 1 Audio Manager
@@ -10,10 +11,17 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // If audio manager is already generated
         if (instance == null) {
+            // Sets this game object to be the instance to exist
             instance = this;
+
+            // Doesn't destroy game object when transitioning scenes
             DontDestroyOnLoad(gameObject);
+
+
             foreach (Sound s in Sounds) {
+                // Adds audio source to sound, and other attributes
                 s.source = gameObject.AddComponent<AudioSource>();
                 s.source.clip = s.clip;
                 s.source.loop = s.loop;
@@ -22,25 +30,32 @@ public class AudioManager : MonoBehaviour
                 s.source.pitch = s.pitch;
             }
 
+            // Plays the background music on start
             PlaySound("Background Sound");
-        } else if (instance != this) {
+        } 
+        else if (instance != this) {
+            // If this is a duplicated Audio Manager, then destroy it
             Destroy(gameObject);
         }
     }
-
-    // Update is called once per frame
-    public void PlaySound(string SoundName)
+    public void PlaySound(string soundName)
     {
+        // Loops through each of the sounds until finding Sound
+        // with name soundName
         foreach (Sound s in Sounds) {
-            if (s.name == SoundName) {
+            if (s.name == soundName) {
+                // Plays the sound
                 s.source.Play();
             }
         }
     }
 
-    public void StopSound(string SoundName) {
+    public void StopSound(string soundName) {
+        // Loops through each of the sounds until finding Sound
+        // with name soundName
         foreach (Sound s in Sounds) {
-            if (s.name == SoundName) {
+            if (s.name == soundName) {
+                // Stops the sound
                 s.source.Stop();
             }
         }
